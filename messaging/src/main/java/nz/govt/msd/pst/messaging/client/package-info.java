22e7e39e12d74c2a25904fe13ed88f8cad5bc58d
adrianmsd <adrian.ross008@msd.govt.nz>
2012-08-15 09:56:22 +1200

    Reorganise so that we don't have a Java application.

--- messaging/src/main/java/nz/govt/msd/pst/messaging/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://msd.govt.nz/client", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package nz.govt.msd.pst.messaging.client;
